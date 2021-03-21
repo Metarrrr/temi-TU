3fd2877cf389e88c0ee7608c67846c6ebfbfeb39
Metarrrr <wirapron8712@gmail.com>
2021-03-22 00:33:18 +0700

    add 5 page **gu phim thai mai dai **

---
+++ app/src/main/java/tar/example/testtar/Arrived2.java
@@ -0,0 +1,14 @@
+package tar.example.testtar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Arrived2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_arrived2);
+    }
+}
